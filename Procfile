22ebbd74a34b9cc1ee7e4b9d32e8a079710c3306
Abhishek Ray <abhishekray07@gmail.com>
2017-09-13 07:50:42 -0700

    First commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fitnotes_visualization.app:create_app\(\) -b 0.0.0.0:$PORT -w 3
---
+++ fitnotes_visualization/public/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Public section, including homepage and signup."""
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+
+from fitnotes_visualization.utils import flash_errors
+
+blueprint = Blueprint('public', __name__, static_folder='../static')
+
+
+@blueprint.route('/', methods=['GET'])
+def home():
+    """Home page."""
+    return render_template('public/home.html')
+
+@blueprint.route('/about/')
+def about():
+    """About page."""
+    return render_template('public/about.html')
